456f8a6d62766259aee2b4c07dfc0711af3050a2
suchi <suchibankoti95@gmail.com>
2023-02-08 21:42:49 +0530

    navbar and main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Mainpage from "./Mainpage";
 import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Navbar />
+      <Mainpage />
     </div>
   );
 }
---
+++ src/Mainpage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Mainpage() {
+  return (
+    <div className="content">
+      <h2>Delicious Food,Delivered To You</h2>
+      <p>
+        hjjjjjjjjjjjjjjjjjjjjjjyyyyyyyyyyyyyyyyyyyrrrrrrrrrrrrrrrrrvvvvvvvvvvvvvvvv
+      </p>
+    </div>
+  );
+}
