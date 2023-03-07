4b1a615399efa6ae3b0dab1aca9fc888842b4af8
suchi <suchibankoti95@gmail.com>
2023-03-07 20:29:51 +0530

    useReducer hook for adding units

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,23 +1,28 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useReducer, useRef } from "react";
 import { dataContext } from "../Context";
 import "./Form.css";
 export default function Form(props) {
   const { manageCart } = useContext(dataContext);
   const { item } = props;
-  const [quant, setQuant] = useState(item.unit);
-
-  function handleQuant(event) {
-    setQuant(event.target.value);
-  }
+  const reducer = (quant, action) => {
+    switch (action) {
+      case "+":
+        return quant + 1;
+      case "-":
+        return quant > 0 ? quant - 1 : 0;
+      case "Add":
+        return quant + 1;
+      case "Change":
+        return;
+      default:
+        console.log("Unexpected action");
+    }
+  };
+  const [quant, dispatch] = useReducer(reducer, item.unit);
 
   return (
-    <div className="form">
-      <form>
-        <div>Amount</div>
-        <label>
-          <input type="text" value={quant} onChange={handleQuant}></input>
-        </label>
-      </form>
+    <div className="quantity">
+      <div>{quant}</div>
       <button className="add-btn" onClick={() => manageCart(item, quant)}>
         Add
       </button>
